a9ac4da8a0d245bc75dd1bca485282ef8558d544
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2025-09-26 11:57:42 +0200

    modification du 26/09/2025 à 11h57

---
+++ database/sql/db_users_queries-update-database.sql
@@ -0,0 +1,45 @@
+/* Modifier la table t_role.
+Ajouter une colonne role_level (nombre entier, obligatoire, valeur par défault: 0).
+La colonne "role_level" permettra de connaitre le niveau d'autorisation du rôle. */
+
+ALTER TABLE t_role
+	ADD COLUMN role_level INT NOT NULL DEFAULT '0';
+	
+
+/* Mettre à jour le niveau de chaque rôle :
+role_id	role_level
+1	->	0
+2	->	9
+3	->	10 */
+
+UPDATE t_role SET role_level=0 WHERE role_id=1;
+UPDATE t_role SET role_level=9 WHERE role_id=2;
+UPDATE t_role SET role_level=10 WHERE role_id=3;
+
+
+/* Ajouter les rôles suivants :
+
+| role_name | role_description | role_level | role_register_code |
+| --- | --- | --- | --- |
+| employé | les salariées | 1 | 7896 |
+| cadre | les managers | 2 | asd44 |
+| dirigeant | La Big Boss | 8 | 4561 | */
+
+INSERT INTO t_role
+(role_name, role_description, role_level, role_register_code)
+VALUES
+('employé', 'les salariés', '1', '7896'),
+('cadre', 'les managers', '2', 'asd44'),
+('dirigeant', 'la big boss', '3', '4561');
+
+
+/*Ajouter les 5 utilisateurs suivants : */
+
+INSERT INTO t_user
+(user_lastname, user_firstname, user_email, user_password, role_id)
+VALUES
+('danloss', 'ella', 'ella.danloss@example.com', '12345', (SELECT role_id FROM t_role WHERE role_name= 'employé')),
+('golay', 'jerry', 'j.golay@example.fr', 'azerty', 'employé'),
+('camant', 'medhi', 'medhi@example.fr', 'password', 'cadre'),
+('javelle', 'aude', 'aj@example.com', '121180', 'employé'),
+('scroute', 'jessica', 'jescr@example.fr', '231297', 'dirigeant');
